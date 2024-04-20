e418fcfa3b03bd03713bc180465595a402755271
David-Toas <showoleolabode.6@gmail.com>
2024-04-20 10:59:54 +0100

    first commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,56 @@
+import BannerImg from "../../assets/women/women2.jpg";
+import { GrSecure } from "react-icons/gr";
+import { IoFastFood } from "react-icons/io5";
+import { GiFoodTruck } from "react-icons/gi";
+
+const Banner = () => {
+  return (
+    <div className="min-h-[550px] flex justify-center items-center py-12 sm:py-0">
+      <div className="container">
+        <div className="grid grid-cols sm:grid-cols-2 gap-6 items-center">
+          {/* Image section */}
+          <div data-aos="zoom-in">
+            <img
+              src={BannerImg}
+              alt=""
+              className="max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover rounded-xl"
+            />
+          </div>
+
+          {/* Text details section */}
+          <div className="flex flex-col justify-center gap-6 sm:pt-0">
+            <h1 data-aos="fade-up" className="text-3xl sm:text-4xl font-bold">
+              Winter Sales upto 50% off
+            </h1>
+            <p
+              data-aos="fade-up"
+              className="text-sm text-gray-500 tracking-wide leading-5"
+            >
+              Discover the ultimate shopping spree with our jaw-dropping 50% discount! From trendy apparel to chic accessories, this limited-time sale is your ticket to fashion-forward bliss. Don’t miss out—shop now and elevate your style without breaking the bank! 
+            </p>
+            <div className="flex flex-col gap-4">
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GrSecure className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-violet-100 dark:bg-violet-400" />
+                <p>Quality Products</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <IoFastFood className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-orange-100 dark:bg-orange-400" />
+                <p>Fast Delivery</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GiFoodTruck className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-green-100 dark:bg-green-400" />
+                <p>Easy Payment method</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GiFoodTruck className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-yellow-100 dark:bg-yellow-400" />
+                <p>Get Offers</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
