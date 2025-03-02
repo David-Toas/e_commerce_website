dfed5fd989c112ac8d42dc2fccd8dbb9321d0c98
David-Toas <111209349+David-Toas@users.noreply.github.com>
2025-03-02 12:08:45 +0100

    Update TopProducts.jsx

--- src/components/TopProducts/TopProducts.jsx
+++ src/components/TopProducts/TopProducts.jsx
@@ -33,7 +33,7 @@ const TopProducts = ({ handleOrderPopup }) => {
     <div>
       <div className="container">
         {/* Header Section */}
-        <div className="text-center mb-10 max-w-[600px] mx-auto">
+        <div className="text-center mb-24 max-w-[600px] mx-auto">
           <p data-aos="fade-up" className="text-sm text-primary">
             Top Rated products for you
           </p>
